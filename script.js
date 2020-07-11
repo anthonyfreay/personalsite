105cac3005ec7d0f53f2fe05c4a3386e99ff5562
AnthonyBFreay <anthonyfreay@gmail.com>
2020-07-11 13:57:34 -0400

    Add loader to gallery images

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 
+const loader = document.createElement('div');
+loader.id = 'loader';
+
+var loadTime;
+
 const allImages = document.querySelectorAll('img');
 allImages.forEach((allImages) => {
   allImages.addEventListener('click', (e) => {
@@ -40,12 +45,24 @@ allImages.forEach((allImages) => {
     var hdImage = str.slice(0, str.length - 4) + '-hd.jpg';
     lightbox.classList.add('active');
     const img = document.createElement('img');
+    img.classList.add('hd-lightbox');
     img.src = hdImage;
     while (lightbox.firstChild) {
       lightbox.removeChild(lightbox.firstChild);
     }
+    lightbox.appendChild(loader);
     lightbox.appendChild(img);
+    loadLightboxImg();
   });
+
+  function loadLightboxImg() {
+    loadTime = setTimeout(showImg, 500);
+  }
+
+  function showImg() {
+    document.getElementsByClassName('hd-lightbox')[0].style.display = 'flex';
+    lightbox.removeChild(loader);
+  }
 });
 
 lightbox.addEventListener('click', (e) => {
