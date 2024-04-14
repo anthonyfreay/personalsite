56437b7807a1831c7cce5957cb5fa46c62558cca
Anthony Brian Freay <anthonyfreay@gmail.com>
2024-04-14 17:13:35 -0400

    Update Black and White Gallery

--- src/Components/Pages/BlackWhite/BlackWhite.jsx
+++ src/Components/Pages/BlackWhite/BlackWhite.jsx
@@ -9,17 +9,26 @@ import Footer from '../../Util/Footer/Footer'
 const BlackWhite = () => {
     const imageData = [
         { alt: "Tyler, the Creator", src: "bw/DSC05585-bw.jpg" },
-        { alt: "Daniel Caesar", src: "bw/DSC01930-bw.jpg" },
-        { alt: "Alone in the parking lot", src: "bw/A7208874-bw.jpg" },
+        { alt: "City from on High", src: "bw/A7202360-bw.jpg" },
         { alt: "Exhausted", src: "bw/DSC01524-bw.jpg" },
-        { alt: "NYC", src: "bw/DSC07973-bw.jpg" },
-        { alt: "J Balvin", src: "bw/RX709059-Edit-bw.jpg" },
-        { alt: "Chelsea Cutler", src: "bw/A7208259-Edit-bw.jpg" },
-        { alt: "Jack Harlow", src: "bw/DSC08655-bw.jpg" },
+        { alt: "Chrysler Building", src: "bw/A7202339-bw.jpg" },
+        { alt: "Goldlink", src: "bw/DSC02678-bw.jpg" },
         { alt: "10th Ave", src: "bw/DSC01358-bw.jpg" },
+        { alt: "Empire State Building", src: "bw/A7202347-bw.jpg" },
+        { alt: "Defund the Police", src: "bw/DSC02466-bw.jpg" },
+        { alt: "Daniel Caesar", src: "bw/DSC01930-bw.jpg" },
+        { alt: "Biking", src: "bw/A7406794-bw.jpg" },
+        { alt: "Gently Trash in NYC", src: "bw/A7406787-bw.jpg" },
+        { alt: "Jack Harlow", src: "bw/DSC08655-bw.jpg" },
+        { alt: "Save the kids", src: "bw/DSC07973-bw.jpg" },
+        { alt: "J Balvin", src: "bw/RX709059-Edit-bw.jpg" },
         { alt: "Over-looked", src: "bw/DSC01515-bw.jpg" },
         { alt: "Saron Crenshaw", src: "bw/A7207520-bw.jpg" },
-        { alt: "Defund the Police", src: "bw/DSC02466-bw.jpg" }
+        { alt: "Arize in Brooklyn", src: "bw/A7201582-Edit-portrait.jpg" },
+        { alt: "Chelsea Cutler", src: "bw/A7208259-Edit-bw.jpg" },
+        { alt: "Ice in my Veins", src: "bw/DSC02743-bw.jpg" },
+        { alt: "Taxi Driver", src: "bw/DSC08988-bw.jpg" },
+        { alt: "Alone in the parking lot", src: "bw/A7208874-bw.jpg" }
     ].map(image => ({
         ...image,
         hdSrc: image.src.replace(/(\.\w+)$/, '-hd$1') // Append '-hd' before the file extension
