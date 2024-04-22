fa625c46b4a93ac4a14fcdc6a53a22774df6792d
Anthony Brian Freay <anthonyfreay@gmail.com>
2024-04-21 20:31:11 -0400

    Update Events gallery

--- src/Components/Util/Footer/Footer.jsx
+++ src/Components/Util/Footer/Footer.jsx
@@ -12,6 +12,7 @@ function Footer({ activePage }) {
                 <Link to="/people" className={`${FooterStyles.pages} ${activePage === 'people' ? FooterStyles.footerActive : ''}`}>People</Link>
                 <Link to="/places" className={`${FooterStyles.pages} ${activePage === 'places' ? FooterStyles.footerActive : ''}`}>Places</Link>
                 <Link to="/cars" className={`${FooterStyles.pages} ${activePage === 'cars' ? FooterStyles.footerActive : ''}`}>Cars</Link>
+                <Link to="/events" className={`${FooterStyles.pages} ${activePage === 'events' ? FooterStyles.footerActive : ''}`}>Events</Link>
                 {/* <Link to="/posters" className={`${FooterStyles.pages} ${activePage === 'posters' ? FooterStyles.footerActive : ''}`}>Posters</Link> */}
                 {/* <Link to="/apparel" className={`${FooterStyles.pages} ${activePage === 'apparel' ? FooterStyles.footerActive : ''}`}>Apparel</Link> */}
                 <Link to="/contact" className={`${FooterStyles.pages} ${activePage === 'contact' ? FooterStyles.footerActive : ''}`}>Contact</Link>
